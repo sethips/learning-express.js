886d6439d2de91b4d00edb94dde075e9f3fe2f8d
Surbhi Oberoi <siyaoberoi93@gmail.com>
2016-12-22 15:22:35 +0530

    handling case for upper lower case route names

--- app2.js
+++ app2.js
@@ -2,19 +2,27 @@ var express = require('express');
 var app = express();
 
 var blocks = {
-	'Fixed': 'Fastened securely',
-	'Movable': 'Capable of moving',
-	'Rotate': 'Rotates in circle'  
+    'Fixed': 'Fastened securely',
+    'Movable': 'Capable of moving',
+    'Rotate': 'Rotates in circle'
 };
 
 app.get('/blocks/:name', function(request, response) {
-	var description = blocks[request.params.name];
-	if(!description) {
-		response.status(404).json('Not found ' + request.params.name);
-	} else {
-		response.json(description + '\n');
-	}
-	
+    // var description = blocks[request.params.name];
+    // if (!description) {
+    //     response.status(404).json('Not found ' + request.params.name);
+    // } else {
+    //     response.json(description);
+    // }
+    var name = request.params.name;
+    var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
+    var description = blocks[block];
+    if (!description) {
+    	response.status(404).json('Not Found ' + request.params.name)
+    } else {
+    	response.json(description);
+    }
+
 });
 
 app.listen(3000, function() {
